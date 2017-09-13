41be1ed9a5182e0bed6ab41d0bf443ed973ee4ae
yudithchuctaya <yudithyesicachuctaya@gmail.com>
2017-09-13 02:05:10 +0200

    avance del trabajo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,84 @@
- function initMap() {
-        
-        var map = new google.maps.Map(document.getElementById("map"), {
-          zoom:5,
-          center : {lat: -9.1191427, lng: -77.0349046},
-          mapTypeControl:false,
-          zoomControl:false,
-          streetViewControl:false,
-        });
+  function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 6
+            });
 
-        // var marker = new google.maps.Marker({
-        //   position: uluru,
-        //   map: map
-        // });
-      
-      function buscar(){
-      	if(navigator.geolocation){
-      		navigator.geolocation.getCurrentPosition(funcionExito,funcionError);
-      	}
+         var infoWindow = new google.maps.InfoWindow({map: map});
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
       }
-      document.getElementById("encuentrame").addEventListener("click",buscar);
-      var latitud,longitud;
-      var funcionExito = function(posicion) {
-      	latitud = position.coords.latitude ;
-      	longitud = position.coords.longitude ;
-      var miUbicacion = new google.maps.Marker({
-      position:{lat:latitud,lng:longitud},
-      animation:google.maps.Animation.DROP,
-      map:map
-    })
-    map.setZooom(18);
-    map.setCenter({lat:latitud, lng:longitud});
 
-    var funcionError = function(error){
-      alert("Tenemos un problema con encontrar tu ubicación")
-       }
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+      }
 
-    }
-}
+
+
+
+
+
+
+
+ // function initMap() {
+        
+ //        var map = new google.maps.Map(document.getElementById("map"), {
+ //          zoom:5,
+ //          center : {lat: -9.1191427, lng: -77.0349046},
+ //          mapTypeControl:false,
+ //          zoomControl:false,
+ //          streetViewControl:false,
+ //        });
+
+ //        // var marker = new google.maps.Marker({
+ //        //   position: uluru,
+ //        //   map: map
+ //        // });
+      
+ //      function buscar(){
+ //      	if(navigator.geolocation){
+ //      		navigator.geolocation.getCurrentPosition(funcionExito,funcionError);
+ //      	}
+ //      }
+ //      document.getElementById("encuentrame").addEventListener("click",buscar);
+ //      var latitud,longitud;
+ //      var funcionExito = function(posicion) {
+ //      	latitud = position.coords.latitude ;
+ //      	longitud = position.coords.longitude ;
+ //      var miUbicacion = new google.maps.Marker({
+ //      position:{lat:latitud,lng:longitud},
+ //      animation:google.maps.Animation.DROP,
+ //      map:map
+ //    })
+ //    map.setZooom(18);
+ //    map.setCenter({lat:latitud, lng:longitud});
+
+ //    var funcionError = function(error){
+ //      alert("Tenemos un problema con encontrar tu ubicación")
+ //       }
+
+ //    }  
+ //  }
+
 
 
    // ******************************SEGUNDA  TRABAJO GUIADO
@@ -46,9 +88,9 @@
 
 
 	 var inputPartida = document.getElementById("punto-partida");
-	 var inputPartida = document.getElementById("punto-partida");
+	 var inputDestino = document.getElementById("punto-destino");
 	 new google.maps.places.Autocomplete(inputPartida);
-	 new google.maps.places.Autocomplete(inputPartida);
+	 new google.maps.places.Autocomplete(inputDestino);
 	 var directionsService = new google.maps.DirectionsService;
      var directionsService = new google.maps.DirectionsRenderer;
 
